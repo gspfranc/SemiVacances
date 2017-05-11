af71b659b38a411ae5f4307d9d84a1c05d3911d3
Francis Gravel St-Pierre <francis@semiweb.ca>
2017-05-11 11:57:55 -0400

    Add validation to Roles

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,6 +1,9 @@
 class Role < ApplicationRecord
   has_and_belongs_to_many :users
 
+  ROLE_LIST = %w(admin gestionnaire report_maker)
+  validates :role, :uniqueness => true, :presence => true, :inclusion=> { :in => ROLE_LIST }
+
   def self.get_role_by_s(role_s)
     return Role.where(role: role_s)
   end
